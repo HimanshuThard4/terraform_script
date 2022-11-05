ae069951e514f31c73aebfab75f357d10c2612d6
HimanshuThard4 <112090864+HimanshuThard4@users.noreply.github.com>
2022-11-05 12:07:44 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+#resource "aws_instance" "app_server" {
+  #ami           = "ami-0e6329e222e662a52"
+  #instance_type = "t2.micro"
+  #key_name = var.key_name
+
+#  tags = {
+#    Name = var.instance_name
+#  }
+#}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
+
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "fortesting" 
+}
+
