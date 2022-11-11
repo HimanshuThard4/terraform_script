c6aede2d79e1e8747a31c3b370922341a5724a89
HimanshuThard4 <112090864+HimanshuThard4@users.noreply.github.com>
2022-11-11 19:14:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "ami_id" {
+    description = "AMI for your Ec2 instance"
+    default     = "ami-0e6329e222e662a52"
+}
+
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
@@ -8,4 +18,6 @@ variable "key_name" {
     description = " SSH keys to connect to ec2 instance" 
     default     =  "jenkins" 
 }
+
+
 
